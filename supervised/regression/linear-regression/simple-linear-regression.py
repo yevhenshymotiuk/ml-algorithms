f0ad765f55707ed7ad2a644a0eea7eb429573a92
Yevhen Shymotiuk <yevhenshymotiuk@gmail.com>
2020-09-22 00:17:21 +0300

    Add LearningProcedureFactory to create specific learning procedure

--- supervised/regression/linear-regression/simple-linear-regression.py
+++ supervised/regression/linear-regression/simple-linear-regression.py
@@ -1,4 +1,5 @@
 import math
+from dataclasses import dataclass
 from enum import Enum
 
 import matplotlib.pyplot as plt
@@ -6,9 +7,60 @@ import numpy as np
 import pandas as pd
 
 
+class OrdinaryLeastSquare:
+    def __call__(self, training_set):
+        m = len(training_set)
+
+        x_mean = sum(training_set.keys()) / m
+        y_mean = sum(training_set.values()) / m
+
+        t1 = sum(
+            (x - x_mean) * (y - y_mean) for x, y in training_set.items()
+        ) / sum((x - x_mean) ** 2 for x in training_set)
+        t0 = y_mean - t1 * x_mean
+
+        return t0, t1
+
+
+@dataclass
+class GradientDescent:
+    alpha: float = 0.00000001
+    iterations: int = 1000
+
+    @staticmethod
+    def h(t0, t1):
+        """Hypothesis"""
+        return lambda x: t0 + t1 * x
+
+    def __call__(self, training_set):
+        m = len(training_set)
+        t0 = t1 = 0
+
+        for i in range(self.iterations):
+            tmp0 = t0 - self.alpha / m * sum(
+                (self.h(t0, t1)(x) - y) for x, y in training_set.items()
+            )
+            tmp1 = t1 - self.alpha / m * sum(
+                (self.h(t0, t1)(x) - y) * x for x, y in training_set.items()
+            )
+            t0 = tmp0
+            t1 = tmp1
+
+        return t0, t1
+
+
 class LearningProcedure(Enum):
     ORDINARY_LEAST_SQUARE = 1
     GRADIENT_DESCENT = 2
+
+
+class LearningProcedureFactory:
+    @classmethod
+    def create_learning_procedure(cls, learning_procedure):
+        if learning_procedure == LearningProcedure.ORDINARY_LEAST_SQUARE:
+            return OrdinaryLeastSquare()
+        elif learning_procedure == LearningProcedure.GRADIENT_DESCENT:
+            return GradientDescent()
 
 
 class SimpleLinearRegression:
@@ -18,22 +70,20 @@ class SimpleLinearRegression:
         self._training_set = training_set
         self._m = len(training_set)
         self.predict = None
-        self.learning_procedure = learning_procedure
+        self._learning_procedure = LearningProcedureFactory.create_learning_procedure(
+            learning_procedure
+        )
         self._t0 = self._t1 = 0
 
-        if learning_procedure == LearningProcedure.GRADIENT_DESCENT:
-            self._alpha = 0.00000001
-            self._iterations = 1000
-
     @staticmethod
-    def _h(t0, t1):
+    def h(t0, t1):
         """Hypothesis"""
         return lambda x: t0 + t1 * x
 
     def _c(self, t0, t1):
         """Cost function"""
         return sum(
-            (self._h(t0, t1)(x) - y) ** 2
+            (self.h(t0, t1)(x) - y) ** 2
             for x, y in self._training_set.items()
         ) / (2 * self._m)
 
@@ -41,7 +91,7 @@ class SimpleLinearRegression:
         """Root mean squared error"""
         return math.sqrt(
             sum(
-                (self._h(t0, t1)(x) - y) ** 2
+                (self.h(t0, t1)(x) - y) ** 2
                 for x, y in self._training_set.items()
             )
             / self._m
@@ -54,47 +104,14 @@ class SimpleLinearRegression:
         return 1 - sum(
             (y_mean - y) ** 2 for y in self._training_set.values()
         ) / sum(
-            (self._h(t0, t1)(x) - y) ** 2
+            (self.h(t0, t1)(x) - y) ** 2
             for x, y in self._training_set.items()
         )
 
-    def _gd(self):
-        """Gradient descent"""
-
-        for i in range(self._iterations):
-            tmp0 = self._t0 - self._alpha / self._m * sum(
-                (self._h(self._t0, self._t1)(x) - y)
-                for x, y in self._training_set.items()
-            )
-            tmp1 = self._t1 - self._alpha / self._m * sum(
-                (self._h(self._t0, self._t1)(x) - y) * x
-                for x, y in self._training_set.items()
-            )
-            self._t0 = tmp0
-            self._t1 = tmp1
-
-        return self._t0, self._t1
-
-    def _ols(self):
-        "Ordinary least square"
-        x_mean = sum(self._training_set.keys()) / self._m
-        y_mean = sum(self._training_set.values()) / self._m
-
-        t1 = sum(
-            (x - x_mean) * (y - y_mean) for x, y in self._training_set.items()
-        ) / sum((x - x_mean) ** 2 for x in self._training_set)
-        t0 = y_mean - t1 * x_mean
-
-        return t0, t1
-
     def train(self):
         """Train a model"""
-        if self.learning_procedure == LearningProcedure.ORDINARY_LEAST_SQUARE:
-            self._t0, self._t1 = self._ols()
-        elif self.learning_procedure == LearningProcedure.GRADIENT_DESCENT:
-            self._t0, self._t1 = self._gd()
-
-        self.predict = self._h(self._t0, self._t1)
+        self._t0, self._t1 = self._learning_procedure(self._training_set)
+        self.predict = self.h(self._t0, self._t1)
 
 
 if __name__ == "__main__":
