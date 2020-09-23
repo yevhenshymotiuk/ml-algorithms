69fe6caaf3295fcae4901095399b63ad98121283
Yevhen Shymotiuk <yevhenshymotiuk@gmail.com>
2020-09-23 19:12:01 +0300

    Substitute SimpleLinearRegresion with LinearRegression with multiple features

--- supervised/regression/linear-regression/simple-linear-regression.py
+++ supervised/regression/linear-regression/simple-linear-regression.py
@@ -3,23 +3,15 @@ from dataclasses import dataclass
 from enum import Enum
 
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import pandas as pd
 
 
-class OrdinaryLeastSquare:
+# TODO: Implement NormalEquation
+class NormalEquation:
     def __call__(self, X, Y):
-        m = len(X)
-
-        x_mean = sum(X) / m
-        y_mean = sum(Y) / m
-
-        t1 = sum((x - x_mean) * (y - y_mean) for x, y in zip(X, Y)) / sum(
-            (x - x_mean) ** 2 for x in X
-        )
-        t0 = y_mean - t1 * x_mean
-
-        return t0, t1
+        pass
 
 
 @dataclass
@@ -28,117 +20,95 @@ class GradientDescent:
     iterations: int
 
     @staticmethod
-    def h(t0, t1):
+    def h(T):
         """Hypothesis"""
-        return lambda x: t0 + t1 * x
+        return lambda X: dot_product(T, X)
 
     def __call__(self, X, Y):
         m = len(X)
-        t0 = t1 = 0
+        T = [0] * len(X[0])
 
         for i in range(self.iterations):
-            tmp0 = t0 - self.alpha / m * sum(
-                (self.h(t0, t1)(x) - y) for x, y in zip(X, Y)
-            )
-            tmp1 = t1 - self.alpha / m * sum(
-                (self.h(t0, t1)(x) - y) * x for x, y in zip(X, Y)
-            )
-            t0 = tmp0
-            t1 = tmp1
+            T = [
+                t
+                - self.alpha
+                / m
+                * sum((self.h(T)(x) - y) * x[i] for x, y in zip(X, Y))
+                for i, t in enumerate(T)
+            ]
 
-        return t0, t1
+        return T
 
 
 class LearningProcedure(Enum):
-    ORDINARY_LEAST_SQUARE = 1
-    GRADIENT_DESCENT = 2
+    GRADIENT_DESCENT = 1
+    NORMAL_EQUATION = 2
 
 
 class LearningProcedureFactory:
     @classmethod
     def create_learning_procedure(
-        cls, learning_procedure, alpha=0.00000001, iterations=1000
+        cls, learning_procedure, alpha=0.0001, iterations=1000
     ):
-        if learning_procedure == LearningProcedure.ORDINARY_LEAST_SQUARE:
-            return OrdinaryLeastSquare()
-        elif learning_procedure == LearningProcedure.GRADIENT_DESCENT:
+        if learning_procedure == LearningProcedure.GRADIENT_DESCENT:
             return GradientDescent(alpha, iterations)
+        else:
+            raise Exception(
+                "You can create only GradientDescent object for now"
+            )
 
 
-class SimpleLinearRegression:
-    """Linear regression with one input variable"""
+def dot_product(A, B):
+    return sum(a * b for a in A for b in B)
+
+
+class LinearRegression:
+    """Linear regression with multiple features"""
 
     def __init__(self, X, Y, learning_procedure):
-        self._X = X
+        self._X = [(1, *x) for x in X]
         self._Y = Y
         self._m = len(X)
         self.predict = None
         self._learning_procedure = learning_procedure
-        self._t0 = self._t1 = 0
+        self._T = [0] * len(X[0])
 
     @staticmethod
-    def h(t0, t1):
+    def h(T):
         """Hypothesis"""
-        return lambda x: t0 + t1 * x
+        return lambda X: dot_product(T, X)
 
-    def _c(self, t0, t1):
+    @property
+    def _c(self):
         """Cost function"""
         return sum(
-            (self.h(t0, t1)(x) - y) ** 2 for x, y in zip(self._X, self._Y)
+            (self.h(self._T)(x) - y) ** 2 for x, y in zip(self._X, self._Y)
         ) / (2 * self._m)
-
-    def _rmse(self, t0, t1):
-        """Root mean squared error"""
-        return math.sqrt(
-            sum((self.h(t0, t1)(x) - y) ** 2 for x, y in zip(self._X, self._Y))
-            / self._m
-        )
-
-    def _cod(self, t0, t1):
-        """Coefficient of determination"""
-        y_mean = sum(Y) / self._m
-
-        return 1 - sum((y_mean - y) ** 2 for y in Y) / sum(
-            (self.h(t0, t1)(x) - y) ** 2 for x, y in zip(self._X, self._Y)
-        )
 
     def train(self):
         """Train a model"""
-        self._t0, self._t1 = self._learning_procedure(self._X, self._Y)
-        self.predict = self.h(self._t0, self._t1)
+        self._T = self._learning_procedure(self._X, self._Y)
+        self.predict = self.h(self._T)
 
 
 if __name__ == "__main__":
-    data = pd.read_csv("headbrain.csv")
+    data = pd.read_csv("student.csv")
 
-    X = data["Head Size(cm^3)"].values
-    Y = data["Brain Weight(grams)"]
+    X1, X2 = data["Math"].values, data["Reading"].values
+    Y = data["Writing"].values
 
     lp = LearningProcedureFactory.create_learning_procedure(
         LearningProcedure.GRADIENT_DESCENT
     )
-    lr = SimpleLinearRegression(X, Y, lp)
+    lr = LinearRegression(list(zip(X1, X2)), Y, lp)
 
     lr.train()
 
-    print(f"{lr._t0=} {lr._t1=}")
+    print(f"{lr._T=} {lr._c=}")
 
-    # Plotting Values and Regression Line
-    max_x = np.max(X) + 100
-    min_x = np.min(X) - 100
-
-    # Calculating line values x and y
-    x = np.linspace(min_x, max_x, 1000)
-    y = lr._t0 + lr._t1 * x
-
-    # Ploting Line
-    plt.plot(x, y, color="#58b970", label="Regression Line")
-    # Ploting Scatter Points
-    plt.scatter(X, Y, c="#ef5423", label="Scatter Plot")
-
-    plt.xlabel("Head Size in cm3")
-    plt.ylabel("Brain Weight in grams")
-    plt.legend()
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    ax.scatter(X1, X2, Y, color="#ef1234")
     plt.show()
 
-    print(lr.predict(4000))
+    print(lr.predict((1, 60, 70)))
